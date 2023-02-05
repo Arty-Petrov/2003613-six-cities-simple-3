9f997345ec0435cb3ccf2bf324ebe877cf0c8f7b
Artyom Petrov <artyom.petrov@gmail.com>
2023-02-05 13:45:46 +0300

    update Exception filter with handle of validation errors

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -7,6 +7,7 @@ import { Feature } from '../types/feature.enum.js';
 import { Location } from '../types/location.type.js';
 import { Lodging } from '../types/lodging.enum.js';
 import { Offer } from '../types/offer.type.js';
+import { ServiceError } from '../types/service-error.enum.js';
 import { ValidationErrorField } from '../types/validation-error-field.type.js';
 
 export const createComment = (row: string) => {
@@ -60,18 +61,6 @@ export const createUser = (row: string) => {
   };
 };
 
-export const getErrorMessage = (error: unknown): string =>
-  error instanceof Error ? error.message : '';
-
-export const createSHA256 = (line: string, salt: string): string => {
-  const shaHasher = crypto.createHmac('sha256', salt);
-  return shaHasher.update(line).digest('hex');
-};
-
-export const createErrorObject = (message: string) => ({
-  error: message,
-});
-
 export const fillDTO = <T, V>(someDto: ClassConstructor<T>, plainObject: V) =>
   plainToInstance(someDto, plainObject, {excludeExtraneousValues: true});
 
@@ -82,8 +71,12 @@ export const multerFilesToDTO = (files: Record<string, Array<Record<string, stri
     const values = files[key].map((item) => item[multerFileField]);
     dto = {...dto, [key]: [...values]};//?
   }
-
   return dto;
+};
+
+export const createSHA256 = (line: string, salt: string): string => {
+  const shaHasher = crypto.createHmac('sha256', salt);
+  return shaHasher.update(line).digest('hex');
 };
 
 export const createJWT = async (algorithm: string, jwtSecret: string, payload: object): Promise<string> =>
@@ -92,6 +85,15 @@ export const createJWT = async (algorithm: string, jwtSecret: string, payload: o
     .setIssuedAt()
     .setExpirationTime('2d')
     .sign(crypto.createSecretKey(jwtSecret, 'utf-8'));
+
+export const createErrorObject = (serviceError: ServiceError, message: string, details: ValidationErrorField[] = []) => ({
+  errorType: serviceError,
+  message,
+  details: [...details]
+});
+
+export const getErrorMessage = (error: unknown): string =>
+  error instanceof Error ? error.message : '';
 
 export const transformErrors = (errors: ValidationError[]): ValidationErrorField[] =>
   errors.map(({property, value, constraints}) => ({
